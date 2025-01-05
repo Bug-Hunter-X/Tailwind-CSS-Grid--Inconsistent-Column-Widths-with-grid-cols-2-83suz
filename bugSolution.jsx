af791842adcb0d8f6951a4bea4b50199cfca0072
BugHunter-X <askzenai@gmail.com>
2025-01-05 17:38:07 +0100

    Add bugSolution.jsx file

---
+++ bugSolution.jsx
@@ -0,0 +1,13 @@
+```javascript
+// bugSolution.jsx
+<div className="bg-gray-100 p-4">
+  <div className="grid grid-cols-2 gap-4">
+    <div className="bg-blue-500 text-white p-2 col-span-1">
+      Div 1
+    </div>
+    <div className="bg-green-500 text-white p-2 col-span-1">
+      Div 2
+    </div>
+  </div>
+</div>
+```
